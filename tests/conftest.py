50b814bc562b0f620fa6807a75ea89a10f100dcb
Joshua Uduagbomen <joshuauduagbomen@MacBookAir.lan>
2026-03-07 10:21:35 +0000

    fix: add pytest and httpx to requirements

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,7 @@
-import pytest
-from httpx import AsyncClient
+from fastapi.testclient import TestClient
 from app.main import app
+import pytest
 
 @pytest.fixture
-async def client():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        yield ac
+def client():
+    return TestClient(app)
